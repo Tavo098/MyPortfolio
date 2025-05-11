c8ec37f7f40c2fd0427a94666ca18706430d1637
Gustavo Pajuelo <gp373@njit.edu>
2025-05-10 20:25:19 -0400

    Added text to cards and links to icons.

--- backend/myportfolio/settings.py
+++ backend/myportfolio/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS =  [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
